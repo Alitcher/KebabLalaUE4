894020dcd55ee1199bbeba72d4f4bbafc09c88c0
alicia22nzd <alicia22nzd@gmail.com>
2023-09-25 23:11:23 +0300

    Initial commit with project files and .gitignore

---
+++ Source/KebabLala/KebabLala.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "KebabLala.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, KebabLala, "KebabLala" );
---
+++ Source/KebabLala/KebabLalaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KebabLalaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KEBABLALA_API AKebabLalaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
